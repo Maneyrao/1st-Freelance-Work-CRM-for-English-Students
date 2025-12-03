6dffc623d519b74b2fff1f9e27232abc08420002
Thiago Maneyro <tmaneyro@gmail.com>
2025-12-03 11:14:54 -0300

    test CORS - confirm Railway running correct main.py

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import students, reminder
 
-app = FastAPI()
-
-origins = ["*"]
+app = FastAPI(title="TEST-ABURRIDONT")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/")
+def root():
+    return {"msg": "API funcionando", "cors": True}
 
 app.include_router(students.router)
 app.include_router(reminder.router)
